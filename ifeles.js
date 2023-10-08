7210457337c601c1a58d50297b7ec3b5db9406ee
Syamsul Buchori Pane <91958966+sul0211@users.noreply.github.com>
2023-10-08 18:57:07 +0700

    Update ifeles.js

--- ifeles.js
+++ ifeles.js
@@ -1,4 +1,5 @@
-let a = 80;
+//ifelse
+let a = 190;
 let b = 120;
 
 if (a > b) {
@@ -10,3 +11,42 @@ if (a > b) {
     console.log("nilai a sama dengan nilai b");
   }
 }
+
+//function
+function kali(a, b) {
+  return a * b;
+}
+let hasil = kali(10, 100);
+console.log("Hasil Perkaliannya adalah =  " + hasil);
+
+//switich-case
+let angka = 90;
+
+switch (angka) {
+  case 100:
+    console.log("Angka tersebut adalah 100");
+    break;
+  case 90:
+    console.log("angkat tersebut adalah 90");
+    break;
+  default:
+    console.log("tidak ada angka yang dimasukkan");
+}
+
+//for
+for (let p = 0; p < 19; p++) {
+  console.log("Kamar di " + (p + 1));
+}
+
+//while-do
+let jenis = 0;
+while (jenis < 5) {
+  console.log("anda memasuki dikamar " + (jenis + 1));
+  jenis++;
+}
+
+let ruangan = 0;
+do {
+  console.log("anda juga telah memasuki dikamar " + (ruangan + 1));
+  ruangan++;
+} while (ruangan < 5);
